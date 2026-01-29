a270964c07fd50d46106032dd886d85a5393db36
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-01-30 05:52:03 +0600

    db setup donej

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
-var cors = require('cors')
-app.use(cors())
+const cors = require('cors')
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egyokrx.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    const db = client.db('competexDB');
+    const userCollections = db.collection('user');
+
+    app.post('/users', async(req, res)=>{
+      const userInfo = req.body;
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World from compete-x server')
@@ -12,3 +65,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
